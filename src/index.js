b8419cce014bf2be954febcf6ea43623b0ba9953
takeshi.fujii <isisaka3000@gmail.com>
2021-02-01 22:08:55 +0900

    bundlerを使わずにjs読み込み

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function componet() {
+  const element = document.createElement('div')
+  const array = ['hello', 'webpack']
+  element.innerHTML = _.join(array, ' ')
+  return element;
+}
+
+document.body.appendChild(componet());
