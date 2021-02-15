b750d03d19a6984cf3e69e5125d6813e10ae21ed
takeshi.fujii <isisaka3000@gmail.com>
2021-02-15 21:02:15 +0900

    apply uglifyjs-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path') 
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 // resolve...絶対パスを生成するメソッド
 const outputPath = path.resolve(__dirname, 'dist')
@@ -52,5 +53,16 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].[hash].css',
     })
-  ]
+  ],
+  optimization: {
+    minimizer: [new UglifyJsPlugin(
+      {
+        uglifyOptions: {
+          compress: {
+            drop_console: true
+          }
+        }
+      }
+    )]
+  }
 }
