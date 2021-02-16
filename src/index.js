84d3f06b7689d6dbecda663133a42c762da94efe
takeshi.fujii <isisaka3000@gmail.com>
2021-02-16 22:23:24 +0900

    install eslint-loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './style.css'
-import './style.scss'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+import './style.scss';
 
-console.log('uglifyjs test.')
+// console.log('uglifyjs test.');
 
 ReactDOM.render(
   <div>React</div>,
-  document.getElementById('root')
-)
+  document.getElementById('root'),
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,78 @@
-const path = require('path') 
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 // resolve...絶対パスを生成するメソッド
-const outputPath = path.resolve(__dirname, 'dist')
+const outputPath = path.resolve(__dirname, 'dist');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: outputPath
+    path: outputPath,
   },
   module: {
-    rules:[
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.(sc|c)ss$/,
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
-          'sass-loader'
-        ]
+          'sass-loader',
+        ],
       },
       {
-        test:/\.(jpe?g|gif|png|svg|ico)$/i,
-        loader:'url-loader',
+        test: /\.(jpe?g|gif|png|svg|ico)$/i,
+        loader: 'url-loader',
         options: {
           limit: 2048,
           name: './images/[name].[ext]',
         },
       },
       {
-        test:/\.html$/,
-        loader: 'html-loader'
-      }
-    ]
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+    ],
   },
   devServer: {
     // 起動時に参照するパス
-    contentBase: outputPath
+    contentBase: outputPath,
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: './src/index.html',
-      filename: './index.html'
+      filename: './index.html',
     }),
     new MiniCssExtractPlugin({
       filename: '[name].[hash].css',
-    })
+    }),
   ],
   optimization: {
     minimizer: [new UglifyJsPlugin(
       {
         uglifyOptions: {
           compress: {
-            drop_console: true
-          }
-        }
-      }
+            drop_console: true,
+          },
+        },
+      },
     ),
-    new OptimizeCSSAssetsPlugin({})
-  ]},
-  devtool: 'eval-source-map'
-}
+    new OptimizeCSSAssetsPlugin({}),
+    ],
+  },
+  devtool: 'eval-source-map',
+};
